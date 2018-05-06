1352cc4c60c976445014005d8c068e8a80bf2ff0
Austin Gill <notgnoshi@gmail.com>
2018-05-06 15:31:49 -0600

    Add code coverage settings to test file

--- runtests.py
+++ runtests.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 import nose
 
-nose.main(argv=['', '--with-doctest'])
+nose.main(argv=['',
+                '--with-doctest',
+                '--with-coverage',
+                '--cover-html',
+                '--cover-erase',
+                '--cover-html-dir=htmlcov'])
